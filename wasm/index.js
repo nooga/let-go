e72ce0b6ed83f8e0148e7cd10651e92f92da265a
Marcin Gasperowicz <xnooga@gmail.com>
2022-12-02 00:09:29 +0000

    Web REPL: Upgrade fastify

--- wasm/index.js
+++ wasm/index.js
@@ -1,19 +1,16 @@
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
 
-// Serve the static assets
-fastify.register(require('fastify-static'), {
+fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, ''),
   prefix: '/'
-})
+});
 
-const start = async () => {
+(async () => {
   try {
-    await fastify.listen(8080, "0.0.0.0")
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-
+    await fastify.listen(8080, "0.0.0.0");
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (error) {
-    fastify.log.error(error)
+    fastify.log.error(error);
   }
-}
-start()
+})();
