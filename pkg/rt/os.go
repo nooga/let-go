e0a70efb57afa9affe9c257037601857a3453543
Marcin Gasperowicz <xnooga@gmail.com>
2023-07-06 11:29:14 +0100

    wips

--- pkg/rt/os.go
+++ pkg/rt/os.go
@@ -7,6 +7,7 @@ package rt
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
@@ -19,6 +20,19 @@ func installOsNS() {
 	execf, err := vm.NativeFnType.Box(exec.Command)
 	tempDir, err := vm.NativeFnType.Box(os.TempDir)
 	args, err := vm.ToLetGo(os.Args)
+	withStdin, err := vm.NativeFnType.Wrap(func(v []vm.Value) (vm.Value, error) {
+		var cmd *exec.Cmd = v[0].Unbox().(*exec.Cmd)
+		s := string(v[1].(vm.String))
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			return vm.NIL, err
+		}
+		go func() {
+			defer stdin.Close()
+			io.WriteString(stdin, s)
+		}()
+		return v[0], nil
+	})
 	if err != nil {
 		panic(fmt.Sprintf("os NS init failed: %e", err))
 	}
@@ -30,6 +44,7 @@ func installOsNS() {
 
 	ns.Def("getenv", getenv)
 	ns.Def("exec", execf)
+	ns.Def("with-stdin", withStdin)
 	ns.Def("temp-dir", tempDir)
 	ns.Def("args", args)
 	RegisterNS(ns)
